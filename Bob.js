cfd71e40f990f196db432014ef72dd0a25af7611
ankita539 <70938626+ankita539@users.noreply.github.com>
2021-01-15 22:05:12 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,24 @@
+class Bob{
+    constructor(x, y, radius){
+        var options = {
+            'isStatic': false,
+            'restitution': 1,
+            'friction': 3,
+            'density': 2
+        }
+        this.body = Matter.Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        fill("black");
+        ellipse(0, 0, this.radius);
+        pop();
+        }
+}
---
+++ Rope.js
@@ -0,0 +1,23 @@
+class Rope{
+    constructor(body1, body2, offsetX, offsetY) {
+     this.offsetX = offsetX;
+     this.offsetY = offsetY;
+      var options = {
+      bodyA: body1,
+      bodyB: body2,
+      point: {x: this.offsetX, y: this.offsetY}
+     }
+     this.rope = Constraint.create(options);
+     World.add(world, this.rope);
+    }  
+    display() {
+      var pointA = this.rope.bodyA.position;
+      var pointB = this.rope.bodyB.position;
+      strokeWeight(4);
+      var posAX = pointA.x;
+      var posAY = pointA.y;
+      var posBX = pointB.x+this.offsetX;
+      var posBY = pointB.y+this.offsetY;
+      line(posAX, posAY, posBX, posBY);
+    }
+  }
